0cc3fdf996f40b0314def20b906507c7e8b0c625
Kunal <mailto.guptakunal@gmail.com>
2022-10-12 00:55:42 +0530

    crud operation with mongoose assignment 2

--- block-BNaacp/models/user.js
+++ block-BNaacp/models/user.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var userSchema = new Schema({
-  name: String,
-  email: String,
-  sports: [String],
-});
+var userSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    sports: [String],
+  },
+  { timestamps: true }
+);
 
 var User = mongoose.model('User', userSchema);
 
